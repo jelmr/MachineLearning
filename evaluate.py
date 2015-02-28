3183d15f2bcc8adf74daf93b62fac79164858f1e
jelmr <jelmer_mulder@hotmail.com>
2015-02-28 23:09:21 +0100

    Add parameter for name of badness plot to evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -7,9 +7,9 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from numpy.random import normal
 
-def calculate_score(results, input):
-    results_reader = csv.reader(results, delimiter=',')
-    input_reader = csv.reader(input, delimiter=',')
+def calculate_score(args):
+    results_reader = csv.reader(args.results, delimiter=',')
+    input_reader = csv.reader(args.input, delimiter=',')
 
     results_header = results_reader.next()
     input_header = input_reader.next()
@@ -22,12 +22,12 @@ def calculate_score(results, input):
 
     crps_score = crps.calc_crps(thresholds, predictions, actuals)
 
-    plot_graph(thresholds, predictions, actuals)
+    plot_graph(thresholds, predictions, actuals, args.plot)
 
     print crps_score
 
 
-def plot_graph(thresholds, predictions, actuals):
+def plot_graph(thresholds, predictions, actuals, name):
     vals = []
     for row, actual in zip(predictions, actuals):
         sum = 0
@@ -43,7 +43,7 @@ def plot_graph(thresholds, predictions, actuals):
     plt.title("Badness frequency plot")
     plt.xlabel("Badness")
     plt.ylabel("Frequency")
-    plt.savefig('graph.png', format='png')
+    plt.savefig(name, format='png')
 
 
 if __name__ == "__main__":
@@ -52,7 +52,9 @@ if __name__ == "__main__":
                         help=("File containing the results of the algorithm."))
     parser.add_argument('input', type=argparse.FileType('r'),
                         help=("Path to the original input file used to generate the results."))
+    parser.add_argument('--plot', default="graph.png", type=argparse.FileType('w'),
+                        help=("Name for the badness plot."))
     args = parser.parse_args()
 
-    calculate_score(args.results, args.input)
+    calculate_score(args)
 
