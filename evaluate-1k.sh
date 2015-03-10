75c553658c3e8417c8ae2a69b3bb74daf67c6c5d
jelmr <jelmer_mulder@hotmail.com>
2015-03-10 10:12:35 +0100

    Add script to run and evaluate all methods.

---
+++ evaluate-1k.sh
@@ -0,0 +1,13 @@
+
+python solution.py --output output-nn.csv input/train-1k.csv nn 
+python solution.py --output output-rr1.csv input/train-1k.csv rr1 
+python solution.py --output output-svr.csv input/train-1k.csv svr
+
+echo "nn:" > cprs.txt
+python evaluate.py --plot nn.png output-nn.csv input/train-1k.csv >>cprs.txt
+echo "rr1:" >> cprs.txt
+python evaluate.py --plot rr1.png output-rr1.csv input/train-1k.csv >>cprs.txt
+echo "svr:" >> cprs.txt
+python evaluate.py --plot svr.png output-svr.csv input/train-1k.csv >>cprs.txt
+
+cat cprs.txt
