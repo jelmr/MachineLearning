a61ddbe97ecf41c9544f0cb2d21a8a054ab24437
jelmr <jelmer_mulder@hotmail.com>
2015-03-12 13:33:38 +0100

    Add boolean classifier.

--- evaluate-1k.sh
+++ evaluate-1k.sh
@@ -1,6 +1,7 @@
 python solution.py --output output-nn.csv input/train-1k.csv input/test-1k.csv nn 
 python solution.py --output output-rr1.csv input/train-1k.csv input/test-1k.csv rr1 
 python solution.py --output output-svr.csv input/train-1k.csv input/test-1k.csv svr
+python solution.py --output output-tc.csv input/train-1k.csv input/test-1k.csv tc
 
 echo "nn:" > cprs.txt
 python evaluate.py --plot nn.png output-nn.csv input/test-1k.csv >>cprs.txt
@@ -8,5 +9,7 @@ echo "rr1:" >> cprs.txt
 python evaluate.py --plot rr1.png output-rr1.csv input/test-1k.csv >>cprs.txt
 echo "svr:" >> cprs.txt
 python evaluate.py --plot svr.png output-svr.csv input/test-1k.csv >>cprs.txt
+echo "tc:" >> cprs.txt
+python evaluate.py --plot tc.png output-tc.csv input/test-1k.csv >>cprs.txt
 
 cat cprs.txt
--- solution.py
+++ solution.py
@@ -3,11 +3,20 @@ import sys
 import logging
 import argparse
 import numpy as np
+import pickle
+import math
 from functools import partial
 import scipy
+
+from sklearn.neighbors import RadiusNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.lda import LDA
 from sklearn.decomposition import PCA
 from sklearn.svm import SVR
-
+from sklearn import svm
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn.linear_model import SGDClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.neural_network import BernoulliRBM
 from sklearn.pipeline import Pipeline
@@ -27,8 +36,6 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 
-
-
 def produce_solution(args):
     writer = csv.writer(args.output, delimiter=',')
     reader_train = csv.reader(args.train, delimiter=',')
@@ -36,16 +43,82 @@ def produce_solution(args):
 
     write_header(writer, NUMBER_OF_PREDICTIONS)
 
-    train_data, expected_train_values = preprocess_data(*split_dataset(np.array(list(reader_train))), remove_high_rr=True)
-    test_data, expected_test_values = preprocess_data(*split_dataset(np.array(list(reader_test))), remove_high_rr=False)
+    train_data, expected_train_values = preprocess_data(*split_dataset(np.array(list(reader_train))), remove_high_rr=True, delete_features=False)
+    test_data, expected_test_values = preprocess_data(*split_dataset(np.array(list(reader_test))), remove_high_rr=False, delete_features=False)
 
     predictor = {
             'rr1': partial(train_rr1, train_data),
             'svr': partial(train_svr, train_data, expected_train_values),
-            'nn': partial(train_nn , train_data, expected_train_values)
+            'nn': partial(train_nn , train_data, expected_train_values),
+            'tc': partial(train_threshold_classifier, train_data, expected_train_values)
             }[args.method]
 
     print_solution_distribution(predictor(), test_data, writer)
+
+def train_threshold_classifier(train_data, expected_train_values):
+    boolean_predictor = train_boolean_predictor(train_data, expected_train_values)
+
+    reg_data, expected_reg_values = zip(*(filter(lambda (row, exp): int(float(exp)) != 0 ,zip(train_data[1:], expected_train_values))))
+    print "Reg data: %d\nReg exp: %d\n" % (len(reg_data), len(expected_reg_values))
+    reg_predictor = train_nn(np.vstack((train_data[0],reg_data)), expected_reg_values)
+
+    return lambda x: [1]*70 if boolean_predictor(x) == 0 else reg_predictor(x)
+
+def evaluate():
+    test_data = np.array(test_data[1:]).astype(np.float)
+    expected_test_values = map(lambda x: 0 if int(float(x)) == 0 else 1, expected_test_values)
+    correct = 0
+    incorrect = 0
+
+
+    logger.info("Boolean classification: \n%s" % (
+            metrics.classification_report(
+            np.array(expected_test_values).astype(np.float).astype(int),
+            boolean_predictor(test_data))))
+
+    for row, expected in zip(test_data, expected_test_values):
+        p = boolean_predictor(row)
+        if (int(p) == int(float(expected))):
+            correct += 1
+        else:
+            incorrect += 1
+
+    print "Correct: %d (%.2f%%) \nIncorrect: %d (%.2f%%)" % (correct, 100.*correct/(incorrect+correct),incorrect, 100.*incorrect/(incorrect+correct))
+
+def train_boolean_predictor(data, expected_values):
+
+    # Keep only entries where RR1 is close to Expected.
+    rr1_id = np.where(data[0] == 'RR1')[0][0]
+    data, expected_values = zip(*(filter(lambda (row, exp): abs(float(row[rr1_id])- float(exp)) < 3 ,zip(data[1:], expected_values))))
+    print "Data: %d\nExp: %d\n" % (len(data), len(expected_values))
+    #data = data[1:]
+
+    expected_values = map(lambda x: 0 if int(float(x)) == 0 else 1, expected_values)
+
+    # Make an equal amount of zeroes and ones.
+    if(True):
+        ones = filter(lambda (x,y): y == 1, zip(data,  expected_values))
+        zeroes = filter(lambda (x,y): y == 0, zip(data,  expected_values))
+        m = min(len(ones), len(zeroes))
+        data, expected_values = zip(*(ones[:m] + zeroes[:m]))
+
+    #clf = RadiusNeighborsClassifier(radius=2.5)
+
+    #clf = KNeighborsClassifier(n_neighbors=13)
+
+    #clf = linear_model.LogisticRegression()
+
+    #logistic = linear_model.LogisticRegression()
+    #rbm = BernoulliRBM(random_state=0, verbose=True)
+    #clf = Pipeline(steps=[('rbm', rbm), ('logistic', logistic)])
+
+    #clf = tree.DecisionTreeClassifier(max_depth=3)
+
+    clf = SGDClassifier(loss="log", penalty="l2")
+
+    clf.fit(data, expected_values)
+    return clf.predict
+
 
 def split_dataset(data):
     expected_id = np.where(data[0] == 'Expected')
@@ -60,9 +133,36 @@ def write_header(writer, n):
     solution_header.extend(['Predicted{0}'.format(t) for t in xrange(0, n)])
     writer.writerow(solution_header)
 
-def preprocess_data(data, expected_values, remove_high_rr=False):
+def preprocess_data(data, expected_values, remove_high_rr=False, delete_features=False):
     logger.info("Starting preprocessing.")
     header = data[0]
+
+        #'Id',  0
+        #'TimeToEnd' 1
+        #'DistanceToRadar' 2
+        #'Composite' 3
+        #'HybridScan' 4
+        #'HydrometeorType' 5
+        #'Kdp' 6
+        #'RR1' 7
+        #'RR2' 8
+        #'RR3' 9
+        #'RadarQualityIndex' 10
+        #'Reflectivity' 11
+        #'ReflectivityQC' 12
+        #'RhoHV' 13
+        #'Velocity' 14
+        #'Zdr' 15
+        #'LogWaterVolume' 16
+        #'MassWeightedMean' 17
+        #'MassWeightedSD' 18
+
+    if delete_features:
+        keep = [7,8,9,17]
+        #keep = [6,7,8,9,10, 11, 12, 13, 14, 15, 16,17]
+        header = header[keep]
+        data = data[:,keep]
+
     # Flatten data by taking mean of measurements.
     # TODO: Use scipy/numpy methods?
     data = map(lambda y:
@@ -88,7 +188,10 @@ def preprocess_data(data, expected_values, remove_high_rr=False):
     # TODO: Use scipy/numpy methods?
     # Remove extreme values (measurement errors)
     data = np.array(map(lambda y:
-            map(lambda x: 0 if x > OUTLIERS_VALUE or x < -OUTLIERS_VALUE else x, y), data))
+        map(lambda x: 0 if x > OUTLIERS_VALUE or x < -OUTLIERS_VALUE else x, y), data))
+
+    #data = np.array(map(lambda y:
+    #        map(lambda x: math.log(abs(x)+0.00001) , y), data))
 
     logger.info("Done with preprocessing.")
 
@@ -98,7 +201,7 @@ def preprocess_data(data, expected_values, remove_high_rr=False):
 def print_solution_distribution(predict, data, writer):
 
     for i, row in enumerate(data[1:]):
-        prediction = predict(row)
+        prediction = predict(np.array(row).astype(np.float))
 
         solution_row = [row[np.where(data[0] == 'Id')][0][0]]
         solution_row.extend(prediction)
@@ -175,12 +278,18 @@ if __name__ == "__main__":
                         help=("The trainig data."))
     parser.add_argument('test', type=argparse.FileType('r'),
                         help=("The test data."))
-    parser.add_argument('method', choices=['rr1', 'svr', 'nn'],
+    parser.add_argument('method', choices=['rr1', 'svr', 'nn', 'tc'],
                         help=("Method to be used to generate the solution."))
     parser.add_argument('--output', type=argparse.FileType('w'),
                         default=sys.stdout,
                         help=("path to an output file, "
                               "defaults to stdout"))
+    parser.add_argument('--export_params', type=argparse.FileType('w'),
+                        default=None,
+                        help=("File to write trained parameters to."))
+    parser.add_argument('--import_params', type=argparse.FileType('r'),
+                        default=None,
+                        help=("File to load trained parameters from."))
 
     args = parser.parse_args()
     produce_solution(args)
